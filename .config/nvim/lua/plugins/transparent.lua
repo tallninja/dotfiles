5429898ac315f0234ae0416cf48348ff52b40205
tallninja <ernestwambua2@gmail.com>
2024-06-24 10:49:05 +0300

    Added transparent plugin

---
+++ .config/nvim/lua/plugins/transparent.lua
@@ -0,0 +1,39 @@
+-- Transparent background
+-- See: https://github.com/xiyaowong/transparent.nvim
+
+return {
+  {
+    "xiyaowong/transparent.nvim",
+    opts = {
+      groups = { -- table: default groups
+        "Normal",
+        "NormalNC",
+        "Comment",
+        "Constant",
+        "Special",
+        "Identifier",
+        "Statement",
+        "PreProc",
+        "Type",
+        "Underlined",
+        "Todo",
+        "String",
+        "Function",
+        "Conditional",
+        "Repeat",
+        "Operator",
+        "Structure",
+        "LineNr",
+        "NonText",
+        "SignColumn",
+        "CursorLine",
+        "CursorLineNr",
+        "StatusLine",
+        "StatusLineNC",
+        "EndOfBuffer",
+      },
+      extra_groups = {}, -- table: additional groups that should be cleared
+      exclude_groups = {}, -- table: groups you don't want to clear
+    },
+  },
+}
